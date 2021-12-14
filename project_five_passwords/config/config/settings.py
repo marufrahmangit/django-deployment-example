8c21e114113967387b05960703fbc8aa2f9a8aac
Munem Maruf <marufmunem@gmail.com>
2021-12-14 11:11:02 +0600

    another

--- project_five_passwords/config/config/settings.py
+++ project_five_passwords/config/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7obik1=9wm--!jzv=4-2qj8q_ysj08+80x&w2kocumo_g6lniw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
